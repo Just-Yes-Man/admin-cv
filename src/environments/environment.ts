c5dbb9ce1633fde2397315234e7f3136f194a610
Just-Yes-Man <andrykgiovanytc@gmail.com>
2025-03-29 01:12:55 -0600

    transeferencia de archivos

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+       production: false,
+       firebase: {
+           apiKey: "AIzaSyB8nksfakXHSoJLHTzM1fN3g5pE0dqbFUM",
+           authDomain: "my-cv-yesman.firebaseapp.com",
+           projectId: "my-cv-yesman",
+           storageBucket: "my-cv-yesman.firebasestorage.app",
+           messagingSenderId: "747644326417",
+           appId: "1:747644326417:web:0516b9d19e9041b08b57b0"
+       }
+};

8b844bb379a97c21f6b0b1a3242b067e951e6105
Andryk Giovany <andrykgiovanytc@gmail.com>
2025-04-02 23:55:15 -0600

    certificates y education

---
+++ src/app/services/education-service/education.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Education } from '../../models/education/education.model';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EducationService {
+
+  private dbPath = '/education';
+
+  educationServiceRef: AngularFirestoreCollection<Education>;
+
+  constructor(private db: AngularFirestore) {
+    this.educationServiceRef = db.collection(this.dbPath);
+  }
+
+  getEducation(): AngularFirestoreCollection<Education> {
+    return this.educationServiceRef;
+  }
+
+  createEducation(myEducation: Education): any {
+    const { id, ...educationNoId } = myEducation; // Elimina el id si está presente
+    return this.educationServiceRef.add(educationNoId);
+  }
+
+  deleteEducation(id?: string): Promise<void> {
+    return this.educationServiceRef.doc(id).delete();
+  }
+}
--- src/app/services/header-service/header.service.ts
+++ src/app/services/header-service/header.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Header } from '../../models/header/header.model';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
 
-  constructor() { }
+  private dbPath = '/header';
+
+  headerServiceRef: AngularFirestoreCollection<Header>;
+
+  constructor(private db: AngularFirestore) {
+    this.headerServiceRef = db.collection(this.dbPath);
+  }
+
+  getHeader(): AngularFirestoreCollection<Header> {
+    return this.headerServiceRef;
+  }
+
+  createHeader(myLanguage: Header): any {
+    const { id, ...languagesNoId } = myLanguage; // Elimina el id si está presente
+    return this.headerServiceRef.add(languagesNoId);
+  }
+
+  deleteHeader(id?: string): Promise<void> {
+    return this.headerServiceRef.doc(id).delete();
+  }
 }
